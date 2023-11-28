1cb751263a1efa8ab5965227548df15d852347ab
designDeffender <designdeffender@gmail.com>
2023-11-28 18:07:52 +0500

    responsive

--- src/app/Components/GChat.jsx
+++ src/app/Components/GChat.jsx
@@ -23,7 +23,7 @@ function GChat() {
           <div className='flex flex-col md:flex-row justify-between md:px-10 lg:px-20'>
             <div className='mt-2.5 md:mt-[89px] md:w-[500px] lg:w-[670px]'>
               <Image src='/images/logo.svg' width={100} height={100} className='hidden md:block'/>
-              <h1 className='text-2xl md:text-[30px] lg:text-[57px] px-0 text-center md:text-start  font-inter font-bold  text-white md:mt-12 lg:mt-[76px] leading-[1.15]'>
+              <h1 className='text-2xl md:text-[30px] lg:text-[40px] xl:text-[57px] px-0 text-center md:text-start  font-inter font-bold  text-white md:mt-12 lg:mt-[76px] leading-[1.15]'>
                 Viete už, ako využiť
                 <br />
                 <span className='underline'> UMELÚ INTELIGENCIU</span>
@@ -37,8 +37,8 @@ function GChat() {
                 <span className='font-bold underline'> ZDARMA E-Book </span>
                 s cieľom rozšíriť vaše obzory využitia AI na Web stránkach a E-Shopoch.
               </p>
-              <div className='flex flex-col lg:flex-row items-center lg:gap-10'>
-                <button className='text-white text-lg font-semibold flex items-center gap-4 rounded-[100px] bg-green button-shadow px-11 py-4'>
+              <div className='flex flex-col xl:flex-row items-center lg:gap-10'>
+                <button className='text-white text-lg font-semibold flex items-center gap-4 rounded-[100px] bg-green button-shadow px-11 py-4 lg:py-6'>
                   Stiahnuť E-Book <Arrow />
                 </button>
                 <button
@@ -54,7 +54,7 @@ function GChat() {
               </div>
             </div>
             <div className='mt-11 md:mt-14 lg:w-[700px] px-9 md:px-0 order-first md:order-last'>
-              <Image src='/images/ebook.png' width={700} height={800} className=' w-[250px] h-[285px] md:w-[600px] lg:w-[700px] md:h-full' />
+              <Image src='/images/ebook.png' width={700} height={800} className=' w-[250px] h-[285px] md:w-[600px] lg:[650px] xl:w-[700px] md:h-[600px] lg:h-full' />
             </div>
           </div>
         </div>

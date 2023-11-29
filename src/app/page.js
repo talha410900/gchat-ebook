68664b16800477b59e2082b4226a3181ca741cb9
appledefender <appledefender@gmail.com>
2023-11-30 00:59:13 +0500

    backend and integration compelted

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import GChat from './Components/GChat'
 
 export default async function Home() {
 
-  let downloadsCount = await fetch('http://localhost:3000/api/get-downloads');
+  let downloadsCount = await fetch('https://gchat-ebook.vercel.app/api/get-downloads');
   downloadsCount = await downloadsCount.json()
 
   return (
